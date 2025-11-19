c0c02b9782b63f68177d2fac8b837ccb90e5c0d1
Praveen-Kumar-Goswami <praveenkumarg8e8@gmail.com>
2025-11-19 20:45:55 +0530

    Delete frontend directory

--- frontend/src/setupAxios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { apiBaseUrl } from './config';
-
-if (apiBaseUrl) {
-  axios.defaults.baseURL = apiBaseUrl;
-}
-
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
